cff98328552b531ceea9ccf23429ea6607820c15
divyamcs24 <divyam.cs24@bmsce.ac.in>
2026-03-04 20:21:24 +0530

    Update week 1 q5.c

--- LAB 1/week 1 q5.c
+++ LAB 1/week 1 q5.c
@@ -1,1 +1,30 @@
+#include<stdio.h>
+#include<limits.h>
+void main()
+{
+    int n,large,sec_large;
+    printf("Enter number of elements: ");
+    scanf("%d",&n);
+    int a[n];
+    printf("Enter array elements: ");
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    sec_large=INT_MIN;
+    large=INT_MIN;
+    for(int i=0;i<n;i++)
+    {
+        if(a[i]>large)
+        {
+            sec_small=large;
+            large=a[i];
+        }
+        if(a[i+1]>sec_large && a[i+1]!=large)
+        {
+            sec_large=a[i+1];
+        }
+    }
+    printf("Second smallest element is %d",sec_large);
+}
 
